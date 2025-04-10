9ee193d5ee5750c0d2d65d3d34fbe3415c7d6456
Stepan Utyupin <stepa1792@gmail.com>
2025-04-10 15:40:10 +0700

    Quartz sync: Apr 10, 2025, 3:40 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -27,8 +27,8 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Search(),
     Component.Darkmode(),
     Component.DesktopOnly(Component.TableOfContents()),
-    Component.DesktopOnly(Component.Backlinks()),
-    // Component.DesktopOnly(Component.Explorer()),
+    //Component.DesktopOnly(Component.Backlinks()),
+    //Component.DesktopOnly(Component.Explorer()),
   ],
   right: [
     // Component.Graph(),

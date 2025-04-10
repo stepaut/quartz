91e3565783ee493c744c3c24d140dba493458172
Stepan Utyupin <stepa1792@gmail.com>
2025-04-10 14:33:53 +0700

    Quartz sync: Apr 10, 2025, 2:33 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Stepan Utyupin",
+    pageTitle: "/recipes",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: false,
--- quartz.layout.ts
+++ quartz.layout.ts
@@ -26,12 +26,14 @@ export const defaultContentPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
+    Component.DesktopOnly(Component.TableOfContents()),
+    Component.Backlinks(),
     // Component.DesktopOnly(Component.Explorer()),
   ],
   right: [
     // Component.Graph(),
-    Component.DesktopOnly(Component.TableOfContents()),
-    Component.Backlinks(),
+    // Component.DesktopOnly(Component.TableOfContents()),
+    // Component.Backlinks(),
   ],
 }
 
